664b1b8b41f0fd131591c1cdc4bff50340c20845
cunhaluisg5 <cunhaluisg5@gmail.com>
2019-06-04 23:07:44 -0300

    Feita modificações na página dashboard

--- src/main/java/br/cesjf/lpwsd/bean/graficoBean.java
+++ src/main/java/br/cesjf/lpwsd/bean/graficoBean.java
@@ -5,6 +5,7 @@
  */
 package br.cesjf.lpwsd.bean;
 
+import br.cesjf.lpwsd.report.Relatorio;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -20,6 +21,38 @@ public class graficoBean implements Serializable{
     
     private BarChartModel bookReserved;
     private BarChartModel bookEmp;
+    //Indica o nome do relatório
+    private String report;
+    private int index;
+    
+    //Gera um relatório
+    public void gerarRelatorio() {
+        Relatorio relatorio = new Relatorio();
+        nameReport();
+        relatorio.setReport(report);
+        relatorio.getRelatorio();
+    }
+    
+    //Seleciona o nome do relatório
+    public void nameReport(){
+        switch(index){
+            case 0:
+                report = "gLivrosReservados";
+                break;
+            case 1:
+                report = "gLivrosEmprestados";
+                break;
+            case 2:
+                report = "gLivrosReservadosCategoria";
+                break;
+            case 3:
+                report = "gLivrosEmprestadosCategoria";
+                break;
+            case 4:
+                report = "gLivrosTotal";
+                break;
+        }
+    }
 
     public BarChartModel getBookReserved() {
         return bookReserved;
@@ -35,6 +68,22 @@ public class graficoBean implements Serializable{
 
     public void setBookEmp(BarChartModel bookEmp) {
         this.bookEmp = bookEmp;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
     
 }
